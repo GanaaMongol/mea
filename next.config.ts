7c62abfaf7a500679d887b6c6921a8e930491787
Ganaa <ganaa7@gmail.com>
2026-09-04 12:23:53 +0800

    fix: include sharp libvips binaries in standalone trace

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   cacheMaxMemorySize: 25 * 1024 * 1024,
+  outputFileTracingIncludes: {
+    '/**': ['./node_modules/.pnpm/@img+*/**/*'],
+  },
 };
 
 export default withPayload(nextConfig);
